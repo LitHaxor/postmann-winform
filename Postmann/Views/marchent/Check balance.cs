9af35cc4590a94bb6c55d491991b0e02458eddb6
MD-SALMAN-HOSSAIN <salman00hossain@gmail.com>
2021-12-01 23:24:27 +0600

    Check Balance Added

---
+++ Postmann/Views/marchent/Check balance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Postmann.Views.marchent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Me.DataGridView1.Rows.Add(textBox1.Text, textBox2.Text)
+        }
+    }
+}

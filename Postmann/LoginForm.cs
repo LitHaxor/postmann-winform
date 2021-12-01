6e1a8152b84477d4ee670959cfb2440daf760dbf
MD-SALMAN-HOSSAIN <salman00hossain@gmail.com>
2021-12-01 11:28:52 +0600

    Update

--- Postmann/LoginForm.cs
+++ Postmann/LoginForm.cs
@@ -30,8 +30,50 @@ namespace Postmann
         private void button2_Click(object sender, EventArgs e)
         {
 
+            this.Hide();
+            Regestration f3 = new Regestration();
+            f3.Show();
         }
 
-        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Focus();
+                errorProvider1.SetError(this.textBox1, "Fill The Field");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox2.Focus();
+                errorProvider1.SetError(this.textBox2, "Fill The Field");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            bool status = checkBox1.Checked;
+            switch (status)
+            {
+                case true:
+                    textBox2.UseSystemPasswordChar = false;
+                    break;
+                default:
+                    textBox2.UseSystemPasswordChar = true;
+                    break;
+            }
+        }
     }
 }
--- Postmann/Regestration.cs
+++ Postmann/Regestration.cs
@@ -17,6 +17,90 @@ namespace Postmann
             InitializeComponent();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+             LoginForm f2 = new LoginForm() ;
+            f2.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Focus();
+                errorProvider1.SetError(this.textBox1, "Fill The Field");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox2.Focus();
+                errorProvider2.SetError(this.textBox2, "Fill The Field");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                textBox3.Focus();
+                errorProvider3.SetError(this.textBox3, "Fill The Field");
+            }
+            else
+            {
+                errorProvider3.Clear();
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox4.Focus();
+                errorProvider4.SetError(this.textBox4, "Fill The Field");
+            }
+            else
+            {
+                errorProvider4.Clear();
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox6.Text))
+            {
+                textBox6.Focus();
+                errorProvider5.SetError(this.textBox6, "Fill The Field");
+            }
+            else
+            {
+                errorProvider5.Clear();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            bool status = checkBox2.Checked;
+            switch (status)
+            {
+                case true:
+                    textBox2.UseSystemPasswordChar = false;
+                    break;
+                default:
+                    textBox2.UseSystemPasswordChar = true;
+                    break;
+            }
+        }
     }
 }

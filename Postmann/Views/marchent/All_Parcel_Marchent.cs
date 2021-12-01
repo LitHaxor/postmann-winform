91cc87937e9f25d62d125f6c2bd007133664b88b
Mubtasim Ihtisham <mubtsaim26@gmail.com>
2021-12-01 23:09:59 +0600

    All Parcel Updates

---
+++ Postmann/Views/marchent/All_Parcel_Marchent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Postmann.Views.marchent
+{
+    public partial class All_Parcel_Marchent : Form
+    {
+        public All_Parcel_Marchent()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

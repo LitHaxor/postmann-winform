4836ee65485722b88f47b7f196f86164193b66b5
MD-SALMAN-HOSSAIN <salman00hossain@gmail.com>
2021-12-01 15:48:09 +0600

    Adding Withdrow  Mathod

---
+++ Postmann/marchent/withdraw.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Postmann.marchent
+{
+    public partial class withdraw : Form
+    {
+        public withdraw()
+        {
+            InitializeComponent();
+        }
+
+        private void withdraw_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
